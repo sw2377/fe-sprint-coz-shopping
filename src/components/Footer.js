8015b8e4219663dd11d4dca5d1fe19a3dc4877f0
sowon_m1 <bsw2377@gmail.com>
2023-07-12 11:14:44 +0900

    footer 컴포넌트 완성[#4]

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <span>개인정보 처리방침 | 이용약관</span>
+      <span>All rights reserved @ Codestates</span>
+    </footer>
+  )
+}
+
+export default Footer;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,17 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Main() {
   return (
     <>
       <Header />
-    
+      <div>내용입니다~~내용입니다~~내용입니다~~</div>
+      <div>내용입니다~~내용입니다~~내용입니다~~</div>
+      <div>내용입니다~~내용입니다~~내용입니다~~</div>
+      <div>내용입니다~~내용입니다~~내용입니다~~</div>
+      <div>내용입니다~~내용입니다~~내용입니다~~</div>
+      <div>내용입니다~~내용입니다~~내용입니다~~</div>
+      <Footer />
     </>
   )
 }
