5387c90bceaa4b187870b961cf983c0a0ea99a83
sowon_m1 <bsw2377@gmail.com>
2023-07-13 19:41:59 +0900

    feat: filter 기능 구현 완료[#11]

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,32 +1,49 @@
 import { useState, useEffect } from 'react';
 import ListItem from '../components/ListItem';
-import './ProductList.css';
 import Filter from '../components/Filter';
+import './ProductList.css';
 
 function ProductList() {
 
-  // App.js 랑 통합가능?
-  const [productList, setProductList] = useState([])
+  // TODO:: App.js 랑 통합가능?
+  const [productList, setProductList] = useState([]);
+  // console.log("🚀 productList", productList);
 
   useEffect(() => {
     getProductList();
   }, []);
 
-  const getProductList = () => {
-    fetch('http://cozshopping.codestates-seb.link/api/v1/products?count=10')
+  const getProductList = (type = "All") => {
+    fetch('http://cozshopping.codestates-seb.link/api/v1/products')
       .then((res) => res.json())
-      .then((data) => setProductList(data))
+      .then((data) => {
+        setProductList(data);
+
+        if (type !== "All") {
+          const filteredData = data.filter((list) => list.type === type);
+          setProductList(filteredData);
+        }
+      })
   }
 
-  console.log("productList", productList);
+  const filterListHandler = (type) => {
+    // console.log("🚀 typeTest", type)
+    getProductList(type);
+  }
   
   return (
     <>
       <main>
-        <Filter />
+        <Filter onFilter={filterListHandler} />
         <section>
           <ul className="listItem">
-            {productList.map((list) => <ListItem key={list.id} {...list} /> )}
+            { productList.map((list) => <ListItem key={list.id} {...list} /> ) }
+            {/* { productList.map((list) => {
+              const updatedList = productList.filter((list) => list.type === "Product")
+              
+            }) } */}
+            
+
           </ul>
         </section>
       </main>
